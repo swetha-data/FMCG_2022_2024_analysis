83389be2550a54f2b2a94e1a09336b71e9a0832a
swetha-data <sweth0698@gmail.com>
2025-11-17 13:39:41 +0530

    Add files via upload

---
+++ sql/01_percentage_change_in_sale_compared.sql
@@ -0,0 +1,11 @@
+percentage change in sale compared to previous year
+with yearly_sale as
+(select datepart(year,date)as year,
+sum(price_unit*units_sold) as sales
+from FMCG_2022_2024
+group by datepart(year,date) --yearly sale
+)
+select *,
+lag(sales,1) over(order by year) as prev_year_sale,
+(sales-lag(sales,1) over(order by year))*100/lag(sales,1) over(order by year) as percentage_change
+from yearly_sale
