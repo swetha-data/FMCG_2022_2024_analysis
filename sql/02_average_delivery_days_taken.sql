6b35127b1b9b239cbc1af5e0dac4deef6a73ea71
swetha-data <sweth0698@gmail.com>
2025-11-17 13:54:07 +0530

    Add files via upload

---
+++ sql/02_average_delivery_days_taken.sql
@@ -0,0 +1,22 @@
+02_average_delivery_days_taken_in_each_region_category
+
+select region,
+category,
+avg(delivery_days) as avg_delivery_days
+from FMCG_2022_2024
+group by region,category
+
+
+Ranking based on average delivery days
+
+with cte as
+(select region,category,
+avg(delivery_days) as avg_delivery_days
+from FMCG_2022_2024
+group by region,category)
+select *,
+dense_rank() over(partition by category order by avg_delivery_days asc) as drnk
+from cte
+
+-- Insight:out of 5 FMCG categories central region makes faster deliveries in 3 - juice,milk
+           and readymeal.
