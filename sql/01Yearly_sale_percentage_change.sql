e6eb3021c4b9bc66c5dcfb942791e2086651c124
swetha-data <sweth0698@gmail.com>
2025-11-17 13:45:33 +0530

    Update and rename 01_percentage_change_in_sale_compared.sql to 01_Yearly_sale_percentage_change.sql

--- sql/01Yearly_sale_percentage_change.sql
+++ sql/01Yearly_sale_percentage_change.sql
@@ -1,4 +1,4 @@
-percentage change in sale compared to previous year
+--Yearly sale analysis with YoY percentage change
 with yearly_sale as
 (select datepart(year,date)as year,
 sum(price_unit*units_sold) as sales
@@ -9,3 +9,4 @@ select *,
 lag(sales,1) over(order by year) as prev_year_sale,
 (sales-lag(sales,1) over(order by year))*100/lag(sales,1) over(order by year) as percentage_change
 from yearly_sale
+--Insight:There was huge increase of sale in year 2023 followed by 1 percentage dip in 2024
