873448c9ce32ea4cba44d6f282edee5f87ca9df7
swetha-data <sweth0698@gmail.com>
2025-11-24 17:07:22 +0530

    Add files via upload

---
+++ sql/05_channel_revenue_monthwise.sql
@@ -0,0 +1,14 @@
+05_channel_revenue_monthwise
+with monthly_rev as
+(select channel,
+datepart(year,date) as year,
+datepart(month,date) as month,
+sum(price_unit*units_sold) as total_revenue
+from FMCG_2022_2024
+group by channel,datepart(month,date),datepart(year,date)) --channel revenue monthwise
+select *
+from
+(select *,
+rank() over(partition by channel order by total_revenue desc) as rnk
+from monthly_rev)A
+where rnk =1
